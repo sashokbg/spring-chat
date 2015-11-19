48c80dc9309ebf394f80bf4f2d7dc6dadfd5f171
sashok_bg <alexander.n.kirilov@gmail.com>
2015-11-19 15:37:18 +0100

    Added log4j2 conf

--- src/main/java/bg/alexander/company/controllers/MessageController.java
+++ src/main/java/bg/alexander/company/controllers/MessageController.java
@@ -1,23 +1,22 @@
 package bg.alexander.company.controllers;
 
-import java.util.concurrent.Callable;
-
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.context.request.async.DeferredResult;
 
 @Controller
 public class MessageController {
-
+	private final Logger log = LogManager.getLogger(MessageController.class);
 	@RequestMapping("/response-body")
-	public @ResponseBody Callable<String> callable() {
-
-		return new Callable<String>() {
-			@Override
-			public String call() throws Exception {
-				Thread.sleep(22000);
-				return "Callable result";
-			}
-		};
+	public @ResponseBody DeferredResult<String> callable() {
+		log.info("Called async method");
+		DeferredResult<String> deferredResult = new DeferredResult<>();
+		
+//		CompletableFuture.supplyAsync(null).whenComplete(action);
+		
+		return deferredResult;
 	}
 }
