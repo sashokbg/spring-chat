4bf4292c57a9c468139a300444ed3691aeff32ca
sashok_bg <alexander.n.kirilov@gmail.com>
2015-11-30 12:26:24 +0100

    Reworked user connection to allow only one pending thread

--- src/main/java/bg/alexander/company/model/UserConnection.java
+++ src/main/java/bg/alexander/company/model/UserConnection.java
@@ -2,6 +2,8 @@ package bg.alexander.company.model;
 
 import java.util.concurrent.ArrayBlockingQueue;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Value;
 
 /**
@@ -16,7 +18,9 @@ public class UserConnection {
 	private ArrayBlockingQueue<Message> messages;
 	private String userName;
 	private String userId;
+	private boolean isWaiting;
 	private boolean isActive;
+	private final Logger log = LogManager.getLogger(UserConnection.class);
 	
 	@Value("${user.connection.keepalive.retries}")
 	private String test;
@@ -37,10 +41,24 @@ public class UserConnection {
 	 * @return the first message of the queue. Waits until one is available
 	 */
 	public Message readMessage(){
+		if(isWaiting){
+			log.error("Someone else is waiting already for a message on this connection. Attemting to liberate connection");
+			try {
+				messages.put(Message.EMPTY);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		}
+		
 		try {
-			return messages.take();
+			isWaiting = true;
+			Message message = messages.take();
+			log.debug("Message taken ["+message+"]");
+			isWaiting = false;
+			return message;
 		} catch (InterruptedException e) {
 			e.printStackTrace();
+			isWaiting = false;
 		}
 		return null;
 	}
@@ -64,8 +82,7 @@ public class UserConnection {
 	public void keepAlive(){
 		keepAliveRetries--;
 		try {
-			Message keepAliveMessage = new Message("","");
-			this.messages.put(keepAliveMessage);
+			this.messages.put(Message.EMPTY);
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
@@ -94,5 +111,13 @@ public class UserConnection {
 	}
 	public void setActive(boolean isActive) {
 		this.isActive = isActive;
+	}
+
+	public boolean isWaiting() {
+		return isWaiting;
+	}
+
+	public void setWaiting(boolean isWaiting) {
+		this.isWaiting = isWaiting;
 	}
 }
